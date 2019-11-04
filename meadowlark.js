921f3d2a4e81d789f48e74e6a9f07c9520990a24
Godfrey-1995 <zhoutf1995@163.com>
2019-11-04 21:00:31 +0800

    modify ch06

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fortune = require('./lib/fortune.js');
+var tours = require('./lib/tours');
 var handlebars = require('express3-handlebars')
     .create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
@@ -18,23 +19,80 @@ app.use(function (req, res, next) {
 //访问静态资源
 app.use(express.static(__dirname + '/public'));
 
+app.get('/headers', function (req, res) {
+    let headers_content = '';
+    for (let name in req.headers) {
+        headers_content += name + ':' + req.headers[name] + '\n';
+    }
+    res.render('headers', {headers_content: headers_content});
+});
+
 //定制首页
 app.get('/', function (req, res) {
     res.render('home');
 });
 
-let fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple."
-];
-
 //定制关于页面
 app.get('/about', function (req, res) {
     res.render('about', {fortune: fortune.getFortune()});
 });
+
+// 提供一个api
+app.get('/api/tours', function (req, res) {
+    // 拼接xml
+    let products = tours.getTours();
+    let toursXml = '<?xml version="1.0"?><tours>' +
+        products.map(function (p) {
+            return '<tour price="' + p.price +
+                '" id="' + p.id + '">' + p.name + '</tour>';
+        }).join('') + '</tours>';
+
+    // 拼接文本
+    let toursText = products.map(function(p){
+        return p.id + ': ' + p.name + ' (' + p.price + ')';
+    }).join('\n');
+
+    res.format({
+        'application/json': function(){
+            res.json(tours);
+        },
+        'application/xml': function(){
+            res.type('application/xml');
+            res.send(toursXml);
+        },
+        'text/xml': function(){
+            res.type('text/xml');
+            res.send(toursXml);
+        },
+        'text/plain': function(){
+            res.type('text/plain');
+            res.send(toursText);
+        }
+    });
+});
+
+app.put('/api/tour/:id', function (req, res) {
+    var p = tours.getTours().some(function(p){ return p.id == req.params.id });
+    if( p ) {
+        if( req.query.name ) p.name = req.query.name;
+        if( req.query.price ) p.price = req.query.price;
+        res.json({success: true});
+    } else {
+        res.json({error: 'No such tour exists.'});
+    }
+});
+
+// app.del('/api/tour/:id', function(req, res){
+//     var i;
+//     for( var i=tours.getTours().length-1; i>=0; i-- )
+//         if( tours[i].id == req.params.id ) break;
+//     if( i>=0 ) {
+//         tours.splice(i, 1);
+//         res.json({success: true});
+//     } else {
+//         res.json({error: 'No such tour exists.'});
+//     }
+// });
 
 // 定制404界面
 app.use(function (req, res) {
