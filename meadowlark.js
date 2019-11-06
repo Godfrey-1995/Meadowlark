dedf4d68582e908819be6d4e5063307f39406d56
Godfrey-1995 <zhoutf1995@163.com>
2019-11-06 18:00:45 +0800

    ch08

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var fortune = require('./lib/fortune.js');
 var tours = require('./lib/tours');
 var handlebars = require('express3-handlebars')
@@ -15,6 +16,8 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 8888);
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // 测试
 app.use(function (req, res, next) {
@@ -51,6 +54,28 @@ app.get('/jquerytest', function(req, res){
 app.get('/nursery-rhyme', function(req, res){
     res.render('nursery-rhyme');
 });
+
+// 注册
+app.get('/newsletter', function (req, res) {
+    res.render('newsletter', {csrf: 'CSRF token goes here'});
+});
+
+// 表单
+app.post('/process', function (req, res) {
+    let form = req.query.form;
+    let csrf = req.body._csrf;
+    let name = req.body.name;
+    let email = req.body.email;
+    console.log('Form (from querystring): ' + form);
+    console.log('CSRF token (from hidden form field): ' + csrf);
+    console.log('Name (from visible form field): ' + name);
+    console.log('Email (from visible form field): ' + email);
+    res.redirect(303, '/thank-you');
+});
+
+app.get('/thank-you', function (req, res) {
+    res.render('thank-you');
+})
 
 app.get('/data/nursery-rhyme', function(req, res){
     res.json({
