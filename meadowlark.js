55c7b10bf88326b9cbf3f31afa2ca28984fdb58e
Godfrey-1995 <zhoutf1995@163.com>
2019-11-07 20:52:50 +0800

    ch08 cookie&session

--- meadowlark.js
+++ meadowlark.js
@@ -4,6 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
+var credentials = require('./credentials');
 var tours = require('./lib/tours');
 var handlebars = require('express3-handlebars')
     .create({defaultLayout: 'main',
@@ -20,6 +21,12 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 8888);
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
 
 // 测试
 app.use(function (req, res, next) {
@@ -30,6 +37,12 @@ app.use(function (req, res, next) {
 
 //访问静态资源
 app.use(express.static(__dirname + '/public'));
+
+app.use(function (req, res, next) {
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
 
 app.get('/headers', function (req, res) {
     let headers_content = '';
@@ -57,9 +70,53 @@ app.get('/nursery-rhyme', function(req, res){
     res.render('nursery-rhyme');
 });
 
+function NewsletterSignUp() {}
+NewsletterSignUp.prototype.save = function(callback) {
+    callback();
+};
+
+app.get('/newsletter', function(req, res){
+    res.render('newsletter');
+});
+
 // 注册
-app.get('/newsletter', function (req, res) {
-    res.render('newsletter', {csrf: 'CSRF token goes here'});
+app.post('/newsletter', function (req, res) {
+    let name = req.body.name || '', email = req.body.email || '';
+    if (name == '' || email == '') {
+        if (req.xhr) return res.json({error: 'Invalid name email address.'});
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was not valid.',
+        }
+        res.redirect(303, '/newsletter/archive');
+    }
+    else {
+        new NewsletterSignUp({name: name, email: email}).save(function (err) {
+            if (err) {
+                if (req.xhr) return res.json({ error: 'Database error.'});
+                req.session.flash = {
+                    type: 'danger',
+                    intro: 'Database error!',
+                    message: 'There was a database error; please try again later.',
+                }
+                res.redirect(303, '/newsletter/archive');
+            }
+            else {
+                if (req.xhr) return res.json({ success: true});
+                req.session.flash = {
+                    type: 'success',
+                    intro: 'Thank you!',
+                    message: 'You have now been signed up for the newsletter.',
+                }
+                res.redirect(303, '/newsletter/archive');
+            }
+        })
+    }
+});
+
+app.get('/newsletter/archive', function (req, res) {
+    res.render('newsletter/archive');
 });
 
 // 表单
@@ -78,16 +135,6 @@ app.post('/process', function (req, res) {
     else {
         res.redirect(303, '/thank-you');
     }
-    return;
-    // let form = req.query.form;
-    // let csrf = req.body._csrf;
-    // let name = req.body.name;
-    // let email = req.body.email;
-    // console.log('Form (from querystring): ' + form);
-    // console.log('CSRF token (from hidden form field): ' + csrf);
-    // console.log('Name (from visible form field): ' + name);
-    // console.log('Email (from visible form field): ' + email);
-    res.redirect(303, '/thank-you');
 });
 
 // 文件上传模版
